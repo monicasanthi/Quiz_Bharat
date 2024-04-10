c845e3fac1506d8f9bddaab86290c815cecc111e
monicasanthi <124545913+monicasanthi@users.noreply.github.com>
2024-04-10 21:37:33 +0530

    Create QuestionAnswer.java

---
+++ QuestionAnswer.java
@@ -0,0 +1,42 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "What is the result of 6 multiplied by 8?",
+            "Which gas is most abundant in Earth's atmosphere?",
+            "If x = 5 and y = 3, what is the value of 2x + 3y?",
+            "Which continent is the largest by land area?",
+            "What is the longest river in the world?",
+            "What is the force that pulls objects towards the center of the Earth?",
+            "What is the process by which plants make their own food?",
+            "Which of the following is a preposition?",
+            "Which ocean is the largest by area?",
+            "Which organ in the human body produces insulin?"
+    };
+    public static String choices [][]={
+            {"12" , "24" , "36" , "48"},
+            {"Oxygen" , "Nitrogen" , "Carbon Dioxide" , "Argon"},
+            {"11" , "13" , "16" , "19"},
+            {"Africa" , "Asia" , "Europe" , "North America"},
+            {"Amazon River" , "Nile River" , "Yangtze River" , "Brahmaputra River"},
+            {"Magnetism" , "Gravity" , "Friction" , "Buoyancy"},
+            {"Respiration" , "Photosynthesis" , "Fermentation" , "Digestion"},
+            {"Run" , "With" , "Book" , "Jump"},
+            {"Atlantic Ocean" , "Indian Ocean" , "Arctic Ocean" , "Pacific Ocean"},
+            {"Liver" , "Kidney" , "Pancreas" , "Stomach"}
+    };
+    public static String correctAnswers []={
+            "48",
+            "Nitrogen",
+            "11",
+            "Asia",
+            "Nile River",
+            "Gravity",
+            "Photosynthesis",
+            "With",
+            "Pacific Ocean",
+            "Pancreas"
+    };
+  
+}
